d07e02474dae16a45c7775f4a85f706ec42f2df0
Felix Angelov <felangelov@gmail.com>
2022-07-22 11:09:23 -0500

    feat: create a very good dart package (#1)

---
+++ lib/src/very_good_dart_package.dart
@@ -0,0 +1,7 @@
+/// {@template very_good_dart_package}
+/// A very good Dart package created by Very Good Ventures.
+/// {@endtemplate}
+class VeryGoodDartPackage {
+  /// {@macro very_good_dart_package}
+  const VeryGoodDartPackage();
+}
