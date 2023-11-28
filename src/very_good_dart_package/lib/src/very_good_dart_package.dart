f745a252a4da99d88f3d05d881816712d8d245a6
Alejandro Santiago <dev@alestiago.com>
2023-11-28 14:10:16 +0000

    refactor: remove generator script (#71)

--- src/very_good_dart_package/lib/src/very_good_dart_package.dart
+++
@@ -1,7 +0,0 @@
-/// {@template very_good_dart_package}
-/// A Very Good Dart package created by Very Good Ventures.
-/// {@endtemplate}
-class VeryGoodDartPackage {
-  /// {@macro very_good_dart_package}
-  const VeryGoodDartPackage();
-}
